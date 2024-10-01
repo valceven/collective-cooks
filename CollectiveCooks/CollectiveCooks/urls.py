6b5eb22cc91f4cd3cee8ea687a7668436e068b9a
valceven <valmykelcevenbolante@gmail.com>
2024-10-01 10:33:46 +0800

    MAJOR STYLE CHANGES
    started initializing tailwind

--- CollectiveCooks/CollectiveCooks/urls.py
+++ CollectiveCooks/CollectiveCooks/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
     path('admin/', admin.site.urls),
     path('auth/',include('accounts.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

2a3ea4697d660678aaf7db87929f6210e5ccd1db
valceven <valmykelcevenbolante@gmail.com>
2024-10-02 15:03:51 +0800

    Added dark mode integration and designs for auth pages

--- CollectiveCooks/Foods/static_src/tailwind.config.js
+++ CollectiveCooks/Foods/static_src/tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
         '../../**/templates/**/*.html',
     ],
     theme: {
-        backgroundColor: theme => ({
-         ...theme('colors'),
-         'light-mode': '#FFFFFF',
-         'dark-mode' : '#121212',
-         'accent1': '#f53803',
-         'accent2': '#f5D020',
-        })
+        extend: {  // Use 'extend' to add custom colors
+            colors: {
+                'light-mode': '#FFFFFF',
+                'dark-mode': '#121212',
+                'accent1': '#f53803',  // Custom accent1 color for text and background
+                'accent2': '#f5D020',  // Custom accent2 color for text and background
+            },
+        },
     },
     darkMode: "class",  // Automatically applies dark mode based on user's system preferences
     plugins: [

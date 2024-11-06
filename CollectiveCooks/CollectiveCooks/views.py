02462fa1ff658d0cb9b3bd281609c8e32c4d6796
valceven <valmykelcevenbolante@gmail.com>
2024-11-07 02:03:11 +0800

    added carousel for top users and provided exact data with responsive layout + resposive footer aswell

--- CollectiveCooks/CollectiveCooks/views.py
+++ CollectiveCooks/CollectiveCooks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Count
+from accounts.models import User
 
 def homepage(request):
-
-    return render(request, 'home.html') 
+    
+    popular_users = User.objects.annotate(follower_count=Count('followers')).order_by('-follower_count')[:5]
+    return render(request, 'home.html', { 'popular_users' : popular_users }) 
 
 def about(request):
   return render(request, 'about.html')
